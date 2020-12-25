9477732460731a1a2613f65592b11975cbaeca44
karen-gishyan <karengishyann@gmail.com>
2020-12-25 15:29:31 +0400

    updates

---
+++ times_series.py
@@ -0,0 +1,20 @@
+from data import *
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller
+
+
+class TimesSeries:
+
+	def __init__(self):
+
+		pass
+
+	def additive_decompose(self):
+
+		pass
+
+	def multiplicative_decompose(self):
+		pass
+
+	def test_stationarity(self):
+		pass
---
+++ tutorial.py
@@ -0,0 +1,16 @@
+def tutorial_function():
+
+	"""
+	Basic intro for the tslearn's series and dataset construction. 
+	"""
+
+	series=[1,3,4,2]
+	formated_tseries=to_time_series(series) # 4 rows and 1 column.
+	print("The shape and type of the series are {}, {}".format(formated_tseries.shape,type(formated_tseries)))
+	#print(formated_tseries)
+
+	second_series=[1,2,4,2,5]
+	formated_dataset=to_time_series_dataset([series,second_series])
+	print("The shape of the dataset is {}".format(formated_dataset.shape)) # 3 dimensional-  2 ts datasets, with 4 rows and 1 column.
+	#print(formated_dataset) 
+
